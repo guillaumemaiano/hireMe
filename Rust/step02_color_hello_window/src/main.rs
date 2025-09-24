b2fec42124afb024c84cef12f6467a3c3f3cdade
Guillaume Maiano @Mars <guillaume@maiano.fr>
2025-09-24 12:42:28 +0200

    Added a temporary font test label before I combine the color and special font.

--- Rust/step02_color_hello_window/src/main.rs
+++ Rust/step02_color_hello_window/src/main.rs
@@ -1,11 +1,32 @@
 use eframe::{egui, App, Frame};
+use eframe::egui::{FontData, FontDefinitions, FontFamily};
 
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions::default();
+    // frame box
     eframe::run_native(
         "Step 01 - Hello Window",
         options,
-         Box::new(|_cc| Ok(Box::new(HelloApp))),
+         Box::new(|cc| {
+             // fonts -- I want CrimsonText
+      let mut fonts = FontDefinitions::default();
+
+            // Add font file
+            fonts.font_data.insert(
+                "my_font".to_owned(),
+                FontData::from_owned(include_bytes!("../assets/CrimsonText-Regular.ttf").to_vec()).into(),
+            );
+
+            // Create a new family alias "MyFont"
+            fonts.families.insert(
+                FontFamily::Name("MyFont".into()),
+                vec!["my_font".to_owned()],
+            );
+
+            cc.egui_ctx.set_fonts(fonts);
+
+            Ok(Box::new(HelloApp))}),
+        
     )
 }
 
@@ -15,6 +36,9 @@ impl App for HelloApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Hello, world!");
+            let special = egui::RichText::new("Special font text")
+                .font(egui::FontId::new(32.0, egui::FontFamily::Name("MyFont".into())));
+            ui.label(special);
             ui.colored_label(egui::Color32::RED, "I’m available for hire");
         });
     }
