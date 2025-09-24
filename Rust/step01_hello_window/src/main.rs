82348544711904866fdb6f923cde4c0ce2f1076a
Guillaume Maiano @Mars <guillaume@maiano.fr>
2025-09-24 12:18:29 +0200

    [DEV] A window that displays a hello world -- and gitignore lock and target files

--- Rust/step01_hello_window/src/main.rs
+++ Rust/step01_hello_window/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::{egui, App, Frame};
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Step 01 - Hello Window",
+        options,
+         Box::new(|_cc| Ok(Box::new(HelloApp))),
+    )
+}
+
+struct HelloApp;
+
+impl App for HelloApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello, world!");
+        });
+    }
 }
