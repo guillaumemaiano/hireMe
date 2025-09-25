513a5ff612e9df33ff90233b2cdccea263a625a0
Guillaume Maiano @Mars <guillaume@maiano.fr>
2025-09-25 15:31:37 +0200

    [DEV] Provides a complete set of locales and relevant code for an it/ru/zh:fr:en app

--- Rust/step04_cjk_cyr_support/src/main.rs
+++ Rust/step04_cjk_cyr_support/src/main.rs
@@ -2,9 +2,11 @@ mod i18n_wrapper;
 use i18n_wrapper::I18n;
 mod hire_me_model;
 use hire_me_model::HireMeModel;
+mod fonts;
+use fonts::setup_fonts;
 
 use eframe::egui::{FontData, FontDefinitions, FontFamily};
-use eframe::{egui, App, Frame};
+use eframe::{App, Frame, egui};
 
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -16,22 +18,7 @@ fn main() -> eframe::Result<()> {
         &format!("{} v{}", APP_NAME, APP_VERSION),
         options,
         Box::new(|cc| {
-            // fonts -- I want CrimsonText
-            let mut fonts = FontDefinitions::default();
-
-            // Add font file
-            fonts.font_data.insert(
-                "my_font".to_owned(),
-                FontData::from_owned(include_bytes!("../assets/CrimsonText-Regular.ttf").to_vec())
-                    .into(),
-            );
-
-            // Create a new family alias "MyFont"
-            fonts.families.insert(
-                FontFamily::Name("MyFont".into()),
-                vec!["my_font".to_owned()],
-            );
-
+            let fonts = setup_fonts();
             cc.egui_ctx.set_fonts(fonts);
 
             Ok(Box::new(HelloApp {
@@ -50,18 +37,19 @@ struct HelloApp {
 impl App for HelloApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(self.i18n.t("intro"));
+            let intro = egui::RichText::new(self.i18n.t("intro"))
+                .font(egui::FontId::new(32.0, egui::FontFamily::Proportional));
+
+            ui.label(intro);
+
             let special = egui::RichText::new(self.i18n.t("hireme"))
-                .font(egui::FontId::new(
-                    32.0,
-                    egui::FontFamily::Name("MyFont".into()),
-                ))
+                .font(egui::FontId::new(32.0, egui::FontFamily::Proportional))
                 .color(egui::Color32::from_rgb(220, 40, 40));
             ui.label(special);
             ui.add_space(8.0);
             ui.separator();
             ui.add_space(4.0);
-/* 
+            /*
             ui.add_space(12.0);
             ui.label(format!(
                 "Audience: {:?}, Language: {}, Fluency: {:?}",
@@ -73,34 +61,34 @@ impl App for HelloApp {
             ui.add_space(12.0);
 
             ui.horizontal(|ui| {
-            egui::ComboBox::from_label("Audience")
-                .selected_text(self.model.current_audience_name())
-                .show_ui(ui, |ui| {
-                    for name in HireMeModel::all_audiences() {
-                        if ui
-                            .selectable_label(self.model.current_audience_name() == name, name)
-                            .clicked()
-                        {
-                            self.model.set_audience_by_name(name);
-                            self.model.set_lang_default();
-                            self.i18n.set_lang("en");
+                egui::ComboBox::from_label("Audience")
+                    .selected_text(self.model.current_audience_name())
+                    .show_ui(ui, |ui| {
+                        for name in HireMeModel::all_audiences() {
+                            if ui
+                                .selectable_label(self.model.current_audience_name() == name, name)
+                                .clicked()
+                            {
+                                self.model.set_audience_by_name(name);
+                                self.model.set_lang_default();
+                                self.i18n.set_lang("en");
+                            }
                         }
-                    }
-                });
-            egui::ComboBox::from_label("Language")
-                .selected_text(self.model.current_lang_name())
-                .show_ui(ui, |ui| {
-                    for lang in self.model.available_languages() {
-                        let name = HireMeModel::lang_name(&lang);
-                        if ui
-                            .selectable_label(self.model.current_lang == lang, name)
-                            .clicked()
-                        {
-                            self.model.set_language(lang.clone());
-                            self.i18n.set_lang(HireMeModel::lang_code(&lang));
+                    });
+                egui::ComboBox::from_label("Language")
+                    .selected_text(self.model.current_lang_name())
+                    .show_ui(ui, |ui| {
+                        for lang in self.model.available_languages() {
+                            let name = HireMeModel::lang_name(&lang);
+                            if ui
+                                .selectable_label(self.model.current_lang == lang, name)
+                                .clicked()
+                            {
+                                self.model.set_language(lang.clone());
+                                self.i18n.set_lang(HireMeModel::lang_code(&lang));
+                            }
                         }
-                    }
-                });
+                    });
             });
 
             ui.hyperlink_to(
